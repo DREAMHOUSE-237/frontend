0b6cc6dd6949ce4bf53f8531de2a58fc69f8bccd
Kieran237-code <kieran.foguem@facsciences-uy1.cm>
2026-05-16 10:08:13 +0100

    Integration frontend/backend

--- vite.config.js
+++ vite.config.js
@@ -8,14 +8,14 @@ export default defineConfig({
     react(),
     tailwindcss()
   ],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://api.16.171.142.15.nip.io',
-        changeOrigin: true,
-        secure: false,
-        rewrite: (path) => path.replace(/^\/api/, '')
-      }
-    }
-  }
+  // server: {
+  //   proxy: {
+  //     '/api': {
+  //       target: 'https://api.16.171.142.15.nip.io',
+  //       changeOrigin: true,
+  //       secure: false,
+  //       rewrite: (path) => path.replace(/^\/api/, '')
+  //     }
+  //   }
+  // }
 })
