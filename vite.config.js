a029b6841c9ba4e4c6fc5344047f40799978d01e
Kieran237-code <kieran.foguem@facsciences-uy1.cm>
2026-05-20 23:46:06 +0100

    Nouvelle fonctionalite frontend

--- vite.config.js
+++ vite.config.js
@@ -7,15 +7,5 @@ export default defineConfig({
   plugins: [
     react(),
     tailwindcss()
-  ],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://api.16.171.142.15.nip.io',
-        changeOrigin: true,
-        secure: false,
-        rewrite: (path) => path.replace(/^\/api/, '')
-      }
-    }
-  }
+  ]
 })
